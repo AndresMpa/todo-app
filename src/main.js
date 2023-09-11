2a3b772ce7a8fbe69562f47ae337ab49e99eaa69
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-11 05:14:02 -0500

    Mobile and themes
    Extending support for mobile, also extending some theme features

--- src/main.js
+++ src/main.js
@@ -1,28 +1,32 @@
 import { loadTasks } from "./preload/previousTodo.js";
+import { preloadTheme } from "./preload/theme.js";
 
 import { createTodo } from "./events/createTodo.js";
 import { filterTodo } from "./events/filterTodo.js";
 import { countTodo } from "./events/countTodo.js";
 import { clearTodo } from "./events/clearTodo.js";
 
+import { updateTheme } from "./util/theme.js";
+
 const addTodo = document.querySelector("#addTodo");
-
 const todoList = document.querySelector("#todoList");
-
 const itemsLeft = document.querySelector("#itemsLeft");
-
 const filters = document.querySelector("#filters");
-
 const clear = document.querySelector("#clear");
 
+const altFilters = document.querySelector("#altFilters");
+
+const themeSwitcher = document.querySelector("#themeSwitcher");
+
 window.addEventListener("load", () => {
+  preloadTheme();
   loadTasks(todoList);
   countTodo(itemsLeft, todoList);
 });
 
-window.addEventListener("click", () => {
-  countTodo(itemsLeft, todoList);
-});
+window.addEventListener("click", () => countTodo(itemsLeft, todoList));
+
+themeSwitcher.addEventListener("click", () => updateTheme(themeSwitcher));
 
 addTodo.addEventListener("keyup", (event) => {
   createTodo(event, todoList);
@@ -30,6 +34,16 @@ addTodo.addEventListener("keyup", (event) => {
 });
 
 filters.addEventListener("click", (event) => {
+  const filter = {
+    All: () => filterTodo("all", todoList, event.target),
+    Active: () => filterTodo("active", todoList, event.target),
+    Completed: () => filterTodo("complete", todoList, event.target),
+  };
+  
+  filter[event.target.textContent]();
+});
+
+altFilters.addEventListener("click", (event) => {
   const filter = {
     All: () => filterTodo("all", todoList, event.target),
     Active: () => filterTodo("active", todoList, event.target),
