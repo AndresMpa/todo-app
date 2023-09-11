081413006d1e0af19c24e89ce161564db573469d
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-11 04:00:00 -0500

    Event handler
    Using a better file system to manage events

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
-const structure = (todo) => `
-<article class="todo">
-  <figure class="todo--icon">
-    <img
-      src="./src/assets/images/icon-check.svg"
-      class="todo--icon--image"
-      alt="Check"
-    />
-  </figure>
-  <p class="todo--description">${msg}</p>
-  <figure class="todo--cross">
-    <img
-      src="./src/assets/images/icon-cross.svg"
-      class="todo--cross--image"
-      alt="Check"
-    />
-  </figure>
-</article>
-`;
+import { loadTasks } from "./preload/previousTodo.js";
+
+import { createTodo } from "./events/createTodo.js";
+import { filterTodo } from "./events/filterTodo.js";
+import { countTodo } from "./events/countTodo.js";
+import { clearTodo } from "./events/clearTodo.js";
+
+const addTodo = document.querySelector("#addTodo");
+
+const todoList = document.querySelector("#todoList");
+
+const itemsLeft = document.querySelector("#itemsLeft");
+
+const filters = document.querySelector("#filters");
+
+const clear = document.querySelector("#clear");
+
+window.addEventListener("load", () => {
+  loadTasks(todoList);
+  countTodo(itemsLeft, todoList);
+});
+
+window.addEventListener("click", () => {
+  countTodo(itemsLeft, todoList);
+});
+
+addTodo.addEventListener("keyup", (event) => {
+  createTodo(event, todoList);
+  countTodo(itemsLeft, todoList);
+});
+
+filters.addEventListener("click", (event) => {
+  const filter = {
+    All: () => filterTodo("all", todoList, event.target),
+    Active: () => filterTodo("active", todoList, event.target),
+    Completed: () => filterTodo("complete", todoList, event.target),
+  };
+
+  filter[event.target.textContent]();
+});
+
+clear.addEventListener("click", () => clearTodo(todoList));
